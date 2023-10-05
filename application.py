23c70be39ea69db66314a6e1513df0189e4ef27e
harikrishna <harikrishna_@hotmail.com>
2023-10-05 11:10:03 +0530

    First commit

---
+++ application.py
@@ -0,0 +1,27 @@
+import pickle
+from flask import Flask, request, jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+app = application
+
+## import ridge regressor and standard scaler pickle
+
+ridge_model = pickle.load(open('model/ridge.pkl','rb'))
+standardscaler = pickle.load(open('model/scaler.pkl','rb'))
+
+@app.route("/")
+def Index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=="POST":
+        pass
+    else:
+        return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(host ="0.0.0.0")
